180a1c45a5890cb8283e943991192c740dea74fa
Chitkarsh Gandhi <chitkarsh@gmail.com>
2024-01-23 16:34:03 +0530

    Add files via upload

---
+++ StayAwake.ps1
@@ -0,0 +1,12 @@
+﻿Clear-Host
+Echo "Keep-alive with NUMLOCK..."
+
+$WShell = New-Object -com "Wscript.Shell"
+
+while ($true)
+{
+  $WShell.sendkeys("{NUMLOCK}")
+  Start-Sleep -Milliseconds 100
+  $WShell.sendkeys("{NUMLOCK}")
+  Start-Sleep -Seconds 240
+}
